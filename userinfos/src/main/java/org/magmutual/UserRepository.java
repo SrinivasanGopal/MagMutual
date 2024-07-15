3d1317d9229992d05cce30ef23ba5ed99fb11286
SrinivasanGopal <50076626+SrinivasanGopal@users.noreply.github.com>
2024-07-15 16:45:07 +0530

    Update UserRepository.java
    Query updated for date range

--- userinfos/src/main/java/org/magmutual/UserRepository.java
+++ userinfos/src/main/java/org/magmutual/UserRepository.java
@@ -13,7 +13,7 @@ public interface UserRepository extends JpaRepository<UserDetails, Long> {
 
 	//Optional<UserDetails> findById(Long id);
 	
-	@Query(value = "SELECT * FROM USERINFOS where datecreated >= ?1 and datecreated <= ?2", nativeQuery = true)
+	@Query(value = "SELECT * FROM USERINFOS where PARSEDATETIME(datecreated,'dd-MM-yyyy') between PARSEDATETIME(?1,'dd-MM-yyyy') and PARSEDATETIME(?2,'dd-MM-yyyy')", nativeQuery = true)
 	List<UserDetails> findByDate(String startDate, String endDate);
 	
 	@Query(value = "select * from userinfos where profession = ?1", nativeQuery = true)
